ab5e13cb0ae768f0a647ae94242cbbcf15997d59
EDVAN ALVES DE OLIVEIRA <25132698@dsn127m1246731.cfp127aluno.local>
2026-03-23 11:26:30 -0300

    feat: init StartScreen

--- app/src/main/java/com/example/mobile_10_exercicio_quiz/MainActivity.kt
+++ app/src/main/java/com/example/mobile_10_exercicio_quiz/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobile_10_exercicio_quiz.screens.StartScreen
 import com.example.mobile_10_exercicio_quiz.ui.theme.MOBILE_10_ExercicioQuizTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MOBILE_10_ExercicioQuizTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    StartScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MOBILE_10_ExercicioQuizTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/mobile_10_exercicio_quiz/screens/StartScreen.kt
+++ app/src/main/java/com/example/mobile_10_exercicio_quiz/screens/StartScreen.kt
@@ -1,11 +1,32 @@
 package com.example.mobile_10_exercicio_quiz.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun StartScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier =
+            modifier
+                .fillMaxSize()
+                .background(Color.Cyan)
+    ) {
+        Text(text = "QUIZATRON 3000")
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = "COMECAR!")
+        }
+    }
+
 
 }
