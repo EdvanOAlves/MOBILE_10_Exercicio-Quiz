7b5dee1c3cc04dbbc60848fb1961b4835630d1e9
EDVAN ALVES DE OLIVEIRA <25132698@dsn127m1246731.cfp127aluno.local>
2026-03-23 11:17:16 -0300

    chore: init

---
+++ app/src/main/java/com/example/mobile_10_exercicio_quiz/screens/ResultScreen.kt
@@ -0,0 +1,2 @@
+package com.example.mobile_10_exercicio_quiz.screens
+
---
+++ app/src/main/java/com/example/mobile_10_exercicio_quiz/screens/StartScreen.kt
@@ -0,0 +1,11 @@
+package com.example.mobile_10_exercicio_quiz.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun StartScreen(modifier: Modifier = Modifier) {
+
+}
